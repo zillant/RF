7812662dac26f9a3282f0053963708764236b11c
zillant <gubaidullin.i@gmail.com>
2020-01-23 22:10:06 +0300

    init lib

---
+++ HackRF/HackRF/hackrflib.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HackRF
+{
+    public class hackrflib
+    {
+        private const string lib = "hackrf";
+        [DllImport(lib, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int hackrf_init();
+    }
+}
